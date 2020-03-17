dc40972bbf293487274c3e4ea9dc111466d62c45
Jon <jong649@gmail.com>
2020-03-17 00:56:47 -0700

    add progress bar and timestamp functionality

--- script.js
+++ script.js
@@ -24,11 +24,22 @@ const updatePlayIcon = () => {
 };
 
 const updateProgress = () => {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 };
 
 const setVideoProgress = () => {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 };
 
 const stopVideo = () => {
